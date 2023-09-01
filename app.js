cd09278aa506f4cc28a8933ee7fea673ef7ad242
Hyunjun-Ko <85072422+Hyunjun-Ko@users.noreply.github.com>
2023-09-01 21:19:02 +0900

    add a basic HTML calculator with buttons for each digit, operators and a display

---
+++ app.js
@@ -0,0 +1,13 @@
+const add = function () {};
+
+const subtract = function () {};
+
+const multiply = function () {};
+
+const divide = function () {};
+
+const operate = function (number1, number2, operator) {};
+
+let number1;
+let number2;
+let operator;
